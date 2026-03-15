0713abdf78c6a9fe480b4413d7f726022cf6fa53
xzen1th <r34lz3n1th@gmail.com>
2026-03-15 12:09:15 +0100

    Added TerminalColor enum class

---
+++ src/main/kotlin/TerminalColor.kt
@@ -0,0 +1,11 @@
+package org.task
+
+/*
+* enum class for colors
+* */
+
+enum class TerminalColor
+{
+    BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE,
+    BRIGHT_BLACK, BRIGHT_RED, BRIGHT_GREEN, BRIGHT_YELLOW, BRIGHT_BLUE, BRIGHT_MAGENTA, BRIGHT_CYAN, BRIGHT_WHITE
+}
