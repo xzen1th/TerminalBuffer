5dae41574ef8536ee6caca7b464757bd32822618
xzen1th <r34lz3n1th@gmail.com>
2026-03-15 12:48:18 +0100

    Added dataclass for cursor position

---
+++ src/main/kotlin/CursorPos.kt
@@ -0,0 +1,7 @@
+package org.task
+
+/*
+* Data class for a cursor position
+* X and Y position need to be mutable if we are going to change them later
+* */
+data class CursorPos(var x: Int, var y: Int)
